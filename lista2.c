91d9b5991ab4d452f862c35fcf513a17e0dca989
MozanielJR <113473875+MozanielJr@users.noreply.github.com>
2023-05-18 14:42:33 -0300

    oquefazer

---
+++ lista2.c
@@ -0,0 +1,20 @@
+/*
+Elabore um programa utilizando as estruturas Professor, Disciplina e Estudante.
+
+Professor (nome, funcional, titulacao (especialista, mestre ou doutor)).
+Disciplina (nome, codigo, carga_horaria, professor_responsavel (escolhidos dentre os professores cadastrados)).
+Estudante (nome, matricula, idade).
+
+Serão cadastrados 3 professores, 2 disciplinas e no máximo 15 estudantes distribuídos entre as duas disciplinas, uma disciplina não poderá ter mais que 10 estudantes matriculados.
+
+Apresente um relatório dos alunos matriculados na primeira disciplina ordenados por idade.
+codigo_disciplina   nome_professor  nome_estudante  idade_estudante
+
+Apresente um relatório dos alunos matriculados na segunda disciplina ordenados por idade em ordem decrescente.
+codigo_disciplina   nome_professor  nome_estudante  idade_estudante
+
+Utilize o bubble sort para ordenação utilizano um procedimento com passagem de parâmetros por referência.
+
+Idade do estudante deverá estar validada entre 16 e 26 por meio de uma função le_valida_idade.
+
+*/
