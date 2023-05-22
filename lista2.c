acc8c9281ab13215372089a9f01be94c3adbcc80
MozanielJR <113473875+MozanielJr@users.noreply.github.com>
2023-05-22 13:56:03 -0300

    relatorios

--- lista2.c
+++ lista2.c
@@ -141,3 +141,74 @@ int main() {
 
     return 0;
 }
+
+
+// /*
+
+// ------------------SISCAMAIS------------
+// CADASTRAR PROFESSOR(1)
+// CADASTRAR DISCIPLINA(2)
+// CADASTRAR ALUNO(3)
+// RELATORIOS (4)
+// SAIR(0)
+// ----------------------------------------
+
+
+
+// ------------------SISCAMAIS------------
+// CADASTRAR PROFESSOR
+
+// NOME:
+// MATRICULA:
+// TITULAÇAO (1- ESPECIALSITA 2-MESTRE 3-DOUTOR):
+// CONTINUAR (S-SIM N-NAO):
+// ----------------------------------------
+
+// ------------------SISCAMAIS------------
+// CADASTRAR DISCIPLINA
+
+// NOME:
+// CODIGO:
+// CARGA HORARIO: 
+// ESCOLHA O PROFESSOR RESPONSAVEL ABAIXO:
+// (1) XXXXXXXXX 
+// (2) XXXXXXXXX
+// (3) XXXXXXXXX
+// CONTINUAR (S-SIM N-NAO):
+// ----------------------------------------
+
+
+// ------------------SISCAMAIS------------
+// CADASTRAR ALUNO
+
+// NOME:
+// MATRICULA:
+// IDADE:
+// ESCOLHA A DISCIPLINA ABAIXO:
+// (1) XXXXXXXXX 
+// (2) XXXXXXXXX
+// CONTINUAR (S-SIM N-NAO):
+// ----------------------------------------
+
+// ------------------SISCAMAIS------------
+// CADASTRAR PROFESSOR(1)
+// CADASTRAR DISCIPLINA(2)
+// CADASTRAR ALUNO(3)
+// RELATORIOS (4)
+// SAIR(0)
+// ----------------------------------------
+
+
+/*
+
+
+Apresente um relatório dos alunos matriculados na primeira disciplina ordenados por idade.
+codigo_disciplina   nome_professor  nome_estudante  idade_estudante
+
+Apresente um relatório dos alunos matriculados na segunda disciplina ordenados por idade em ordem decrescente.
+codigo_disciplina   nome_professor  nome_estudante  idade_estudante
+
+*/
+
+
+//  * /
